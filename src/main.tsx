33ca7257fcfed975d500ff8f4bfd437641bd8c77
dkim08 <dkim191108@gmail.com>
2026-01-28 14:38:25 +0500

    add 4 fire-cards

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import FireCard from "./components";
+import App from "./components/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-      < FireCard
-        title="Card Title"
-        contentType="listNumbered"
-        items={["First item", "Second item", "Third item"]}
-        sx={{
-          backgroundColor: '#7A3A2E',
-          textColor: '#FFFFFF',
-          borderRadius: '16px',
-          padding: '20px',
-          maxWidth: '400px',
-        }}
-        
-      />
+    <App />
   </React.StrictMode>
 );
